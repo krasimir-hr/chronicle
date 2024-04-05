af0388648735a13ada7afd234e8682316cab6ad0
krasimir-hr <krasimir.hr@icloud.com>
2024-04-05 21:16:04 +0300

    fixed a few other things.

--- chronicled/settings.py
+++ chronicled/settings.py
@@ -99,7 +99,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
