8a511965cb11633b6f3766aff094fba5871e1a0f
krasimir-hr <krasimir.hr@icloud.com>
2024-04-05 21:24:13 +0300

    fixed a few other things.

--- chronicled/settings.py
+++ chronicled/settings.py
@@ -99,10 +99,11 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
+
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
