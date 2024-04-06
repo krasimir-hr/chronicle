54d35fbda7906860aea3122516198b60627b3315
krasimir-hr <krasimir.hr@icloud.com>
2024-04-06 18:51:17 +0300

    commit

--- chronicled/settings.py
+++ chronicled/settings.py
@@ -1,18 +1,15 @@
 import os
 from pathlib import Path
 from django.urls import reverse_lazy
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-!oz2ecxti=un6jrqyfzooy!11vb@j8ed8hmcl7mhq5ap9grz5n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-if DEBUG is True:
-    ALLOWED_HOSTS = ['*']
-
-else:
-    ALLOWED_HOSTS = ['chronicled.azurewebsites.net', 'chronicled.krasye.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -62,29 +59,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chronicled.wsgi.application'
 
-if not DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST"),
-            "PORT": os.environ.get("POSTGRES_PORT"),
-        }
-    }
+database_url = os.environ.get("DATABASE_URL")
 
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "chronicle-db",
-            "USER": "postgres",
-            "PASSWORD": "password",
-            "HOST": "127.0.0.1",
-            "PORT": "5432",
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=database_url,
+        conn_max_age=600,
+    )
+}
+
+
+# DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "NAME": "chronicle-db",
+#             "USER": "postgres",
+#             "PASSWORD": "password",
+#             "HOST": "127.0.0.1",
+#             "PORT": "5432",
+#         }
+#     }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
