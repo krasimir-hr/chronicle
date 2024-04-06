aefdbf639bced75da79aad423fcd7fa3d7a2faa3
krasimir-hr <krasimir.hr@icloud.com>
2024-04-06 19:37:44 +0300

    fixed a few things

--- chronicled/settings.py
+++ chronicled/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
