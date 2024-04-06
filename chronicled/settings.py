04623b0a820db6a7b1e59e675a79a400f98ffd75
krasimir-hr <krasimir.hr@icloud.com>
2024-04-06 18:10:57 +0300

    added csfr_trusted_origins

--- chronicled/settings.py
+++ chronicled/settings.py
@@ -131,4 +131,4 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 LOGIN_URL = 'login'
 
-CSRF_TRUSTED_ORIGINS = ['https://chronicled.azurewebsites.net', 'https://chronicled.krasye.com/']
+CSRF_TRUSTED_ORIGINS = ['https://www.chronicled.krasye.com', 'https://chronicled.krasye.com']
